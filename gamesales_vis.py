b769617880604139f8ee5aa5f0e7b3f52337e2cb
AustenHsiao <Austenhsiao@gmail.com>
2021-08-03 09:56:58 -0700

    q1 bubble chart (publisher v #titles)

--- gamesales_vis.py
+++ gamesales_vis.py
@@ -15,12 +15,11 @@ class Vis:
     # 1 Parameter:  filename (as csv)
     # Reads CSV into dataframe, stored in object
     def __init__(self, dataset):
-        self.df = pd.read_csv(dataset).drop(
-            columns=["Unnamed: 0", "img"])
+        self.df = pd.read_csv(dataset).drop(columns=["Unnamed: 0", "img"])
 
-    # Creates a barchart that contains average critic score by publisher for publishers with titles >= thresh
+    # Creates a barchart that contains average critic score by publisher for publishers with number of titles >= thresh
     def query1(self, thresh=0):
-        count               = self.df[["publisher", "critic_score"]].\
+        count               = self.df[["publisher", "critic_score", "user_score"]].\
                               groupby(["publisher"], as_index=False).\
                               size()
         average             = self.df[["publisher", "critic_score"]].\
@@ -30,7 +29,7 @@ class Vis:
         query1              = pd.merge(average, count, how='left', on="publisher")
         query1_filtered     = query1[query1["size"] > thresh].reset_index().drop(columns="index")
         ax1                 = sns.barplot(x="publisher", y="critic_score",
-                              data=query1_filtered, palette="bright")
+                              data=query1_filtered.head(20), palette="bright")
         for bar in ax1.patches:
             ax1.annotate(format(bar.get_height(), '.2f'),
             (bar.get_x() + bar.get_width() / 2,
@@ -40,7 +39,11 @@ class Vis:
         plt.xticks(rotation=45)
         plt.yticks(range(0, 11))
         ax1.set(xlabel="Publisher", ylabel="Average Critic Score", title="Average Critic Score by Publisher")
-        plt.show()        
+        plt.show(block=True)
+        ax2                 = sns.scatterplot(x="publisher", y="size", size="critic_score", data=query1_filtered.head(20), sizes=(50, 200))
+        plt.xticks(rotation=45)
+        plt.show(block=True)
+        
 
 if __name__ == "__main__":
     Vis(DATAFILE).query1(thresh=200)
